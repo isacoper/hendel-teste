db52c3507c570b50028ed637f450873afd8bd3ff
Isabela Correia <belacoper@gmail.com>
2020-05-10 12:26:11 -0300

    configurações do devise

--- app/models/company_user.rb
+++ app/models/company_user.rb
@@ -2,7 +2,7 @@ class CompanyUser < ApplicationRecord
   validates :name, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, 
          :recoverable, :rememberable, :validatable
   belongs_to :company, optional: true
 end
